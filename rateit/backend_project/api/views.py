2c73f0722c6b3d190848a06a4db15c9470f8d332
Aman Gupta <amanrgupta.456@gmail.com>
2025-05-13 23:10:52 +0530

    aman

--- rateit/backend_project/api/views.py
+++ rateit/backend_project/api/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Role
+from .serializers import RoleSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+class RoleViewSet(ViewSet):
+    def list(self, request):
+        roles = Role.objects.all()
+        serializer = RoleSerializer(roles, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        role = get_object_or_404(Role, pk=pk)
+        serializer = RoleSerializer(role)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = RoleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def update(self, request, pk=None):
+        role = get_object_or_404(Role, pk=pk)
+
+
+        # Logic to delete a specific role
+def dictfetchall(cursor):
+    "Return all rows from a cursor as a dict"
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
